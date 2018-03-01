e528303371a1fe6273085c44a1a3b3e1c6ca92af
Jake Zaia <jzaia@stuy.edu>
2018-03-01 14:47:25 -0500

    Finish work

--- circle.js
+++ circle.js
@@ -1,27 +1,31 @@
 var stophammertime = function() {
-  window.cancelAnimationFrame(reqID);
+  if (!timer)
+    return;
+  clearInterval(timer);
+  timer = null;
 };
 
 var clear = function() {
-  ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
-  ctx.beginPath();
+  slate.innerHTML = '';
 };
 
 var animateCircle = function() {
   stophammertime();
-
-  ctx.fillStyle = "lightsteelblue";
+  clear();
 
   var rad = 0;
   var radMode = 0;
 
-  var drawCircle = function() {
-    clear(); //hacky fix
-    ctx.arc(ctx.canvas.width/2, ctx.canvas.height/2, rad, 0, TAU);
-    ctx.stroke();
-    ctx.fill();
+  var circ = slate.appendChild(document.createElementNS(NS, 'circle'));
+  circ.setAttribute("cx", WIDTH/2);
+  circ.setAttribute("cy", HEIGHT/2);
+  circ.setAttribute("fill", "lightsteelblue");
+  circ.setAttribute("stroke", "black");
 
-    if (rad > ctx.canvas.width/2)
+  var drawCircle = function() {
+    circ.setAttribute('r', rad);
+
+    if (rad > WIDTH/2)
       radMode = 1;
     if (rad <= 1)
       radMode = 0;
@@ -30,48 +34,53 @@ var animateCircle = function() {
       rad-=2;
     else
       rad+=2;
-    reqID = window.requestAnimationFrame(drawCircle);
-    if (reqID%10 == 0)
-      console.log("ReqID: " + reqID);
   };
 
-  drawCircle();
+  timer = setInterval(drawCircle, 20);
 };
 
 var animateDVD = function() {
   stophammertime();
+  clear();
 
   var dx = Math.floor(Math.random() * 5 + 1);
   var dy = Math.floor(Math.random() * 5 + 1);
-  var x = Math.floor(Math.random() * (ctx.canvas.width - img.width*2) + img.width + 1);
-  var y = Math.floor(Math.random() * (ctx.canvas.height - img.height*2) + img.height + 1);
+  var x = Math.floor(Math.random() * (WIDTH - img.width*2) + img.width + 1);
+  var y = Math.floor(Math.random() * (HEIGHT - img.height*2) + img.height + 1);
 
-
-
+  var dvd = slate.appendChild(document.createElementNS(NS, 'image'));
+  dvd.setAttribute('x', x);
+  dvd.setAttribute('y', y);
+  dvd.setAttribute('href', 'david.png');
+  dvd.setAttribute('width', img.width);
+  dvd.setAttribute('height', img.height);
+  //<image xlink:href="firefox.jpg" x="0" y="0" height="50px" width="50px"/>
   var drawDVD = function() {
-    clear();
-    ctx.drawImage(img,x,y);
 
-    reqID = window.requestAnimationFrame(drawDVD);
-    if (reqID%10 == 0)
-      console.log("ReqID: " + reqID);
-
-    if (Math.abs(y-ctx.canvas.height) < img.height + 1 || y < 1)
+    if (Math.abs(y-HEIGHT) < img.height + 1 || y < 1)
       dy*=-1;
-    if (Math.abs(x-ctx.canvas.width) < img.width + 1 || x < 1)
+    if (Math.abs(x-WIDTH) < img.width + 1 || x < 1)
       dx*=-1;
 
+    dvd.setAttribute('x', x);
+    dvd.setAttribute('y', y);
+
+    console.log('hui');
 
     x+=dx;
     y+=dy;
   };
 
-  drawDVD();
+  timer = setInterval(drawDVD, 20);
 };
 
-var ctx = document.getElementById("iamthecanvas").getContext('2d');
+var NS = "http://www.w3.org/2000/svg";
 var TAU = 2*Math.PI; //TAU!!!
-var reqID = 0;
+var slate = document.getElementById("slate");
+var timer = null;
+var WIDTH = 500; //cheaty
+var HEIGHT = 500; //cheaty
+
 
 var img = new Image();
 img.src="david.png";
